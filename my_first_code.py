b01a26d9738d34bd83ad2a381b77024363d96fc6
MiffySiu <87935956+MiffySiu@users.noreply.github.com>
2022-03-23 00:23:15 +0800

    1.2.4 Practical Activity

---
+++ my_first_code.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.2.4 Practical activity: Write my first line of code
+
+# In[1]:
+
+
+# this is my first line of code
+
+print('My name is Miffy.')
+print('The date is 22 March 2022')
+
+
+# In[1]:
+
+
+# write a program to analyse real estate data
+
+# total sales from December
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+total_sales = a + b + c + d + e
+
+print(total_sales)
+
+
+# In[2]:
+
+
+# average sales price for the month of December
+
+avg_sales = total_sales / 5
+
+print(avg_sales)
+
+
+# In[3]:
+
+
+# property price for property C
+
+property_price_c = (c - 6500)
+
+print(property_price_c)
+
